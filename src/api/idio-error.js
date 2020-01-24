ec78546abee123f1ed054a5d63ff2f8127b8a502
Daniel Starns <danielstarns@hotmail.com>
2020-01-24 19:30:24 +0000

    Release 3.0.0 bugfix: idio-error's name should be the same as its constructors name.

--- src/api/idio-error.js
+++ src/api/idio-error.js
@@ -1,13 +1,16 @@
 class IdioError extends Error {
     constructor(message, { code = 500 } = {}) {
         super(message);
+        this.name = this.constructor.name;
 
         this.message = message;
+        this.error = message;
         this.code = code;
-        this.name = `idio-graphql-error`;
         this.readme =
             "https://github.com/danstarns/idio-graphql/blob/master/README.md";
         this.chat = "https://spectrum.chat/idio-graphql";
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
