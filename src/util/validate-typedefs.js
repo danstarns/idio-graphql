28f505f20798fa9aa2553b52054c4790d17e6366
Daniel Starns <danielstarns@hotmail.com>
2020-02-09 10:47:27 +0000

    Refactor: appliance to instance

--- src/util/validate-typedefs.js
+++ src/util/validate-typedefs.js
@@ -2,24 +2,24 @@ const { parse } = require("graphql/language");
 const { printWithComments } = require("graphql-toolkit");
 const IdioError = require("../api/idio-error.js");
 
-function validateTypeDefs(appliance, metadata) {
+function validateTypeDefs(instance, metadata) {
     let ast;
 
     try {
-        ast = parse(appliance.typeDefs);
+        ast = parse(instance.typeDefs);
     } catch (error) {
         throw new IdioError(
-            `${metadata._Constructor.name} with name: '${appliance.name}' could not parse typeDefs \n${error}.`
+            `${metadata._Constructor.name} with name: '${instance.name}' could not parse typeDefs \n${error}.`
         );
     }
 
     const foundDefinition = ast.definitions
         .filter((def) => def.kind === metadata.kind)
-        .find((def) => def.name.value === appliance.name);
+        .find((def) => def.name.value === instance.name);
 
     if (!foundDefinition) {
         throw new IdioError(
-            `${metadata._Constructor.name} with name: '${appliance.name}' should contain a ${metadata.kind} called '${appliance.name}'.`
+            `${metadata._Constructor.name} with name: '${instance.name}' should contain a ${metadata.kind} called '${instance.name}'.`
         );
     }
 
