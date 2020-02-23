bcb86096b9de2d62bceb7cdafa93119653cc61df
Daniel Starns <danielstarns@hotmail.com>
2020-02-23 19:36:46 +0000

    Build: removed extra require, is this causing the jsdocs to dissaper?

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,23 @@
-"use strict";
-
 /* eslint-disable import/no-extraneous-dependencies */
 require("core-js/stable");
 
 require("regenerator-runtime/runtime");
 
-const api = require("./api/index.js");
+const {
+    IdioDirective,
+    IdioEnum,
+    IdioInterface,
+    IdioScalar,
+    IdioUnion
+} = require("./api/appliances/index.js");
 
-module.exports = api;
+module.exports = {
+    GraphQLNode: require("./api/graphql_node/graphql-node.js"),
+    combineNodes: require("./api/combine_nodes/combine-nodes.js"),
+    GraphQLGateway: require("./api/graphql_gateway/graphql-gateway.js"),
+    IdioDirective,
+    IdioEnum,
+    IdioInterface,
+    IdioScalar,
+    IdioUnion
+};
