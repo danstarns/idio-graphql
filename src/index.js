c4785fbeced5be61896d980217b732fae1face79
Daniel Starns <danielstarns@hotmail.com>
2020-02-23 19:49:56 +0000

    Fix: remove extra require

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@
 require("core-js/stable");
 require("regenerator-runtime/runtime");
 
-const api = require("./api/index.js");
+/* eslint-disable global-require */
+const {
+    IdioDirective,
+    IdioEnum,
+    IdioInterface,
+    IdioScalar,
+    IdioUnion
+} = require("./api/appliances/index.js");
 
-module.exports = api;
+module.exports = {
+    GraphQLNode: require("./api/graphql_node/graphql-node.js"),
+    combineNodes: require("./api/combine_nodes/combine-nodes.js"),
+    GraphQLGateway: require("./api/graphql_gateway/graphql-gateway.js"),
+    IdioDirective,
+    IdioEnum,
+    IdioInterface,
+    IdioScalar,
+    IdioUnion
+};
