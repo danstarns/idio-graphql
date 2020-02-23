208f0c8095443f71ec1bd499232526eff7a6bf83
Daniel Starns <danielstarns@hotmail.com>
2020-02-23 19:04:54 +0000

    Build: 3.0.1

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,23 +1,26 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { (0, _defineProperty2.default)(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
 const GraphQLNode = require("./graphql_node/graphql-node.js");
 
 const combineNodes = require("./combine_nodes/combine-nodes.js");
 
 const GraphQLGateway = require("./graphql_gateway/graphql-gateway.js");
 
-const appliances = require("./appliances/index.js");
+const {
+  IdioDirective,
+  IdioEnum,
+  IdioInterface,
+  IdioScalar,
+  IdioUnion
+} = require("./appliances/index.js");
 
-module.exports = _objectSpread({
+module.exports = {
   GraphQLNode,
   combineNodes,
-  GraphQLGateway
-}, appliances);
+  GraphQLGateway,
+  IdioDirective,
+  IdioEnum,
+  IdioInterface,
+  IdioScalar,
+  IdioUnion
+};
--- dist/index.js
+++ dist/index.js
@@ -5,24 +5,6 @@ require("core-js/stable");
 
 require("regenerator-runtime/runtime");
 
-const {
-  GraphQLNode,
-  combineNodes,
-  GraphQLGateway,
-  IdioEnum,
-  IdioScalar,
-  IdioDirective,
-  IdioInterface,
-  IdioUnion
-} = require("./api/index.js");
+const api = require("./api/index.js");
 
-module.exports = {
-  GraphQLNode,
-  combineNodes,
-  IdioScalar,
-  IdioEnum,
-  IdioDirective,
-  GraphQLGateway,
-  IdioInterface,
-  IdioUnion
-};
+module.exports = api;
