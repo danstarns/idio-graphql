42824151f59f6e6e8c5d4a9867a5f7dde05a5278
Daniel Starns <danielstarns@hotmail.com>
2020-03-14 11:43:37 +0000

    Build: remove dist from source

--- dist/util/is-function.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function isFunction(value) {
-  return value && (Object.prototype.toString.call(value) === "[object Function]" || typeof value === "function" || value instanceof Function);
-}
-
-module.exports = isFunction;
