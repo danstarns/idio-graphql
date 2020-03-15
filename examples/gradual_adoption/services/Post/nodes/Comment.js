604b23ba6cccf5ee5f2649c0b660caa41058e300
Daniel Starns <danielstarns@hotmail.com>
2020-03-15 16:00:12 +0000

    Dev: update gradual adoption examples

--- examples/gradual_adoption/services/Post/nodes/Comment.js
+++ examples/gradual_adoption/services/Post/nodes/Comment.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { gql } = require("apollo-server");
-const { GraphQLNode } = require("idio-graphql");
+const { GraphQLNode } = require("../../../../../src");
 
 const { comments } = require("../../../../data/index.js");
 
