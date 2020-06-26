a36d746bf3ac3ec427ae38d288a14d03e1c170b1
Daniel Starns <danielstarns@hotmail.com>
2020-06-26 08:14:20 +0100

    refactor: change to index files paragdime

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+const GraphQLNode = require("./graphql_node/graphql-node.js");
+const combineNodes = require("./combine_nodes/combine-nodes.js");
+const GraphQLGateway = require("./graphql_gateway/graphql-gateway.js");
+const appliances = require("./appliances/index.js");
+
+module.exports = {
+    GraphQLNode,
+    combineNodes,
+    GraphQLGateway,
+    appliances
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 /* eslint-disable global-require */
 require("regenerator-runtime/runtime");
 
-const GraphQLNode = require("./api/graphql_node/graphql-node.js");
-const combineNodes = require("./api/combine_nodes/combine-nodes.js");
-const GraphQLGateway = require("./api/graphql_gateway/graphql-gateway.js");
-const IdioDirective = require("./api/appliances/idio-directive.js");
-const IdioEnum = require("./api/appliances/idio-enum.js");
-const IdioInterface = require("./api/appliances/idio-interface.js");
-const IdioScalar = require("./api/appliances/idio-scalar.js");
-const IdioUnion = require("./api/appliances/idio-union.js");
-const GraphQLType = require("./api/appliances/graphql-type.js");
+const {
+    GraphQLNode,
+    combineNodes,
+    GraphQLGateway,
+    appliances
+} = require("./api/index.js");
 
 module.exports = {
     GraphQLNode,
     combineNodes,
     GraphQLGateway,
-    IdioDirective,
-    IdioEnum,
-    IdioInterface,
-    IdioScalar,
-    IdioUnion,
-    GraphQLType
+    ...appliances
 };
